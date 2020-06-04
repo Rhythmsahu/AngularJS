fe92bdacd826e57811648f4dfeb22fdb095406af
Rhythm <rhythmasahu@gmail.com>
2020-06-04 20:45:49 +0530

    module1-sol

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -5,14 +5,22 @@
 
   function LunchCheckController($scope) {
     $scope.list="";
+    $scope.message="";
+
     $scope.checkIfTooMuch=function () {
-      $scope.message=checkIfTooMuch($scope.list)
+      $scope.message=checkIfTooMuch($scope.list);
+      if($scope.message==="Enjoy"){
+        document.getElementByClass('form-group message').style.color="green";
+      }
     }
 
     function checkIfTooMuch(str) {
       var arr=str.split(",");
       var m="";
-      if(arr.length<=3){
+      if(str===""){
+        m="Please enter data first"
+      }
+      else if(arr.length<=3){
         m="Enjoy";
     }
     else {
