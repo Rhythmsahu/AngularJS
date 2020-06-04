58e79d7d55806becc2c87c4badb20860f5c75a2c
Rhythm <rhythmasahu@gmail.com>
2020-06-04 16:38:37 +0530

    second

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 (function () {
-//'use strict';
+  'use strict';
 
-x="hello";
+  angular.module('NameCalculator',[])
+  .controller('NameCalculatorController',NameCalculatorController);
 
-  angular.module('myFirstApp',[])
-  .controller('MyFirstController',function () {
+  function NameCalculatorController($scope,$filter,$injector) {
+    $scope.name="";
+    $scope.total=0;
 
-  })
+    $scope.display=function () {
+      $scope.total=calculateValue($scope.name);
+      //$scope.total=totalNumeric;
+    }
+
+    $scope.upper=function () {
+      var upCase=$filter('uppercase');
+      $scope.name=upCase($scope.name);
+    }
+    };
+
+    function calculateValue(string) {
+      var totalString=0;
+      for (var i = 0; i < string.length; i++) {
+        totalString+=string.charCodeAt(i);
+      }
+      return totalString;
+    };
+
 })();
